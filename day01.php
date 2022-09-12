8ce5cb5ff77d366d1e09da42748151d96d0cd384
lxsxuesong <songsong@songsongdeMacBook-Pro.local>
2022-09-12 08:05:49 +0800

    PHP测试

---
+++ day01.php
@@ -0,0 +1,11 @@
+<?php
+
+// $sum = 0;
+// for ($i=1;$i<=100;$i++) {
+//     $sum = $sum + $i;
+// }
+// echo '1到100所有整数和为'.$sum;
+//echo  'hello ,my students';
+echo  date('Y-m-d H:i:s /n');
+echo "\r";
+echo date("Y-m-d H:i:s", time()+8*60*60);
